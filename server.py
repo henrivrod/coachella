718121ff664823b08a1cb1b0e7312786e1232d35
Henri Vrod <hcv2106@columbia.edu>
2022-03-31 20:34:21 -0400

    Trying to debug sqlalchemy

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ Read about it online.
 """
 
 import os
-from flask.ext.sqlalchemy import *
+from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
